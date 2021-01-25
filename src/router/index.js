e3c47b0ca8db35f560421ff5400664a58a97d0ba
mata.chuan <th2160@Nyu.edu>
2021-01-25 14:46:28 +0800

    first commit

---
+++ src/router/index.js
@@ -0,0 +1,16 @@
+import { Route, Switch } from "react-router-dom";
+import React from "react";
+import Home from "../pages/Home";
+import Login from "../pages/Login";
+import Register from "../pages/Register";
+
+
+const Router =() =>(
+    <switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/login" component={Login} />
+        <Route exact path="/register" component={Register} />
+    </switch>
+);
+
+export default Router;
